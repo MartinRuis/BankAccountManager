d0c59f0a635ef89f4dea6bf379547499c126630a
MartinRuis <ruismartin98@gmail.com>
2019-04-09 16:44:52 +0200

    Added Models

---
+++ BankAccountManager.Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManager.Models
+{
+    public class Expense
+    {
+        //properties
+        public int ExpenseID { get; private set; }
+        public decimal ExpenseAmount { get; private set; }
+        public ExpenseType Type { get; private set;}
+        public string ExpenseDescription { get; private set; }
+        public DateTime ExpenseDate { get; private set; }
+    }
+}
--- BankAccountManager.Models/ExpenseType.cs
+++ BankAccountManager.Models/ExpenseType.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace BankAccountManager.Models
 {
-    public class Class1
+    public enum ExpenseType
     {
+        Withdrawal,
+        Deposit
     }
 }
